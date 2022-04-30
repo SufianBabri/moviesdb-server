1e6da5d75827be3c17603fb614a700b8770460c7
Sufian <sufian.babri@gmail.com>
2022-04-30 12:55:34 +0500

    Clean up

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,6 @@ const auth = require('../middleware/auth');
 const bcrypt = require('bcrypt');
 const _ = require('lodash');
 const { User, validate } = require('../models/user');
-const debug = require('debug')('vidly:verbose');
 const express = require('express');
 const router = express.Router();
 
@@ -18,11 +17,6 @@ router.post('/', async (req, res) => {
 	let user = await User.findOne({ email: req.body.email });
 	if (user) return res.status(400).send('User already registered');
 
-	// user = new User({
-	// 	name: req.body.name,
-	// 	email: req.body.email,
-	// 	password: req.body.password,
-	// });
 	user = new User(_.pick(req.body, 'name', 'email', 'password'));
 	const salt = await bcrypt.genSalt(10);
 	user.password = await bcrypt.hash(user.password, salt);
