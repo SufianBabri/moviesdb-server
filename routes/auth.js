6099d7de0549769dc92f958bb43808e2ce16c93f
Sufian <sufian.babri@gmail.com>
2022-05-05 09:32:51 +0500

    Fixed issue where variable was used but not declared.

--- routes/auth.js
+++ routes/auth.js
@@ -6,30 +6,34 @@ const express = require('express');
 const router = express.Router();
 
 router.post('/', async (req, res) => {
-    const { error } = validate(req.body);
-    if (error) return res.status(400).send(error.details[0].message);
+	const { error } = validate(req.body);
+	if (error) return res.status(400).send(error.details[0].message);
 
-    user = await User.findOne({
-        email: req.body.email,
-    });
-    debug('Result', user);
+	const user = await User.findOne({
+		email: req.body.email,
+	});
+	debug('Result', user);
 
-    if (!user) return res.status(400).send('Invalid email or password');
+	if (!user) return res.status(400).send('Invalid email or password');
 
-    const validPassword = await bcrypt.compare(req.body.password, user.password);
-    if (!validPassword) return res.status(400).send('Invalid email or password');
+	const validPassword = await bcrypt.compare(
+		req.body.password,
+		user.password
+	);
+	if (!validPassword)
+		return res.status(400).send('Invalid email or password');
 
-    const token = user.generateAuthToken();
+	const token = user.generateAuthToken();
 
-    res.send(token);
+	res.send(token);
 });
 
 function validate(req) {
-    const schema = {
-        email: Joi.string().min(5).max(255).required().email(),
-        password: Joi.string().min(5).max(255).required()
-    }
-    return Joi.validate(req, schema);
+	const schema = {
+		email: Joi.string().min(5).max(255).required().email(),
+		password: Joi.string().min(5).max(255).required(),
+	};
+	return Joi.validate(req, schema);
 }
 
 module.exports = router;
